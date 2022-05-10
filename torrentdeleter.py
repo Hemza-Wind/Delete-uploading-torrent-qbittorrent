644fa816859739aa21f5bfd8406fcde6cd963513
spicyramen <hemzawind@gmail.com>
2022-05-10 14:38:22 +0200

    Update torrentdeleter.py

--- torrentdeleter.py
+++ torrentdeleter.py
@@ -2,7 +2,9 @@ import qbittorrentapi
 import time
 from qbittorrentapi import Client
 
-client = Client(host='localhost:8080', username='admin', password='<insert your password here>')
+client = Client(
+    host="localhost:8080", username="admin", password="<insert your password here>"
+)
 
 print("checking for torrents that are uploading")
 print("torrents that are deleted:")
@@ -12,11 +14,10 @@ try:
         for torrent in client.torrents_info():
             # check if torrent is uploading
             if torrent.state_enum.is_uploading:
-                client.torrents_delete(delete_files=False, torrent_hashes =torrent.hash)
-                print(f'{torrent.name} is deleted...')
+                client.torrents_delete(delete_files=False, torrent_hashes=torrent.hash)
+                print(f"{torrent.name} is deleted...")
         # decide how long to wait for next check in seconds
-        time.sleep(10) 
+        time.sleep(10)
 
 except KeyboardInterrupt:
     pass
-
